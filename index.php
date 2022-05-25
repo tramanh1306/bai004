b347660de7ec70d353526b2d56df51b36f7c5e71
tramanh1306 <100921382+tramanh1306@users.noreply.github.com>
2022-05-25 15:42:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$a = 5;
+$b = 3;
+$c = $a + $b;
+echo "Tong: $tong <br>";
+
+$hieu = $a - $b;
+echo "Tong: $hieu <br>";
+
+$tich = $a * $b;
+echo "Tich: $tich <br>";
+
+$thuong = $a / $b;
+echo "Thuong: $thuong <br>";
+
+$sodu = $a % $b;
+echo "So du: $sodu <br>";
+
+
+?>
+
+
+
+
